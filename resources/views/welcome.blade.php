8ed08f2064ab8d14940ea8090063b9e126dd2efa
oleg vasilev <oleg.vasilev1991@gmail.com>
2018-04-22 12:48:01 +0200

    221

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Спортивне орієнтування11
+                    Спортивне орієнтування22
                 </div>
 
 
