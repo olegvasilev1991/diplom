ed0658fd2b42ec9d0d72c5a9e5476244f0aa4961
oleg vasilev <oleg.vasilev1991@gmail.com>
2018-06-21 02:34:34 +0300

    model fix

--- app/Participant.php
+++ app/Participant.php
@@ -11,6 +11,6 @@ class Participant extends Model
         'id_card','turn_id', 'first_name', 'last_name', 'birth', 'phone', 'email', 'sex', 'photo'
     ];
     public function points(){
-        return $this->hasMany('App\Points','card_id')->orderBy('kp','asc');
+        return $this->hasMany('App\Points','card_id','id_card')->orderBy('kp','asc');
     }
 }
